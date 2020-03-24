310e9afa3915e1ff0fe470e007405d2596752cfd
Rafael Ketsetsides <rketsetsides@gmail.com>
2020-03-24 19:52:19 -0400

    Use linter before running extension

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 file="$1"
 platform="$2"
 dir=$(mktemp -d)
-unzip "$1" -d "$dir" && web-ext run -s "$dir" -t "$platform" && rm -r "$dir"
+unzip -q "$1" -d "$dir" && web-ext lint -s "$dir" &&  web-ext run -s "$dir" -t "$platform" && rm -r "$dir"
