644e24ae827c274aacdeb94eb971cc82ab5ee9ad
Rafael Ketsetsides <rketsetsides@gmail.com>
2020-05-04 14:27:29 -0400

    Measure performance

--- src/test.js
+++ src/test.js
@@ -2,25 +2,21 @@ import {getEngineSpec} from "./storage.js";
 
 function performSearch(engine, searchTerm) {
     var output;
+    var begin_time = Date.now();
     browser.runtime.sendMessage({open_engine: {url: engine.baseurl.replace("{searchTerms}", encodeURIComponent(searchTerm)), timeout: engine.timeout}})
         .then(tabid => {
             let port = browser.tabs.connect(tabid);
             port.postMessage(engine);
-            return function(callback_succ, callback_err) {
-                port.onMessage.addListener(function(r) {
-                    if (r.resp) {
-                        callback_succ(r.resp);
-                    }
-                    else {
-                        callback_err(r.err || r);
-                    }
-                });
-            };
-        }).then(function(get_resp) {
-            get_resp(function(resp) {
-                output = resp;
+            port.onMessage.addListener(function(r) {
+                if (r && r.resp) {
+                    output = {resp: r.resp, time: Date.now() - begin_time};
+                    console.log(output);
+                }
+                else {
+                    console.log(r.err || r);
+                }
             });
-        }, console.log);
+        });
     const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
     return wait(engine.timeout).then(() => output);
 }
@@ -38,14 +34,18 @@ getEngineSpec().then(function(engines) {
         eng.appendChild(eng_name);
         eng.appendChild(eng_stat);
         prom = prom.then(() => performSearch(engine, "sample"))
-            .then(resp => {
-                console.log(engine.name, resp);
-                if (resp && resp.length) {
-                    eng_stat.innerText = "OK " + resp.length;
+            .then(r => {
+                console.log(engine.name, r);
+                if (r && r.resp && r.resp.length) {
+                    eng_stat.innerText = "OK " + r.resp.length + " " + r.time + " ms";
                     eng_stat.style.color = "green";
                 }
+                else if (r) {
+                    eng_stat.innerText = "NO RESULTS";
+                    eng_stat.style.color = "red";
+                }
                 else {
-                    eng_stat.innerText = "FAIL";
+                    eng_stat.innerText = "TIMED OUT";
                     eng_stat.style.color = "red";
                 }
             });
